5556830c8add767ec1b2a336b359c9a92bde1c9e
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 15:46:44 -0800

    add GetTotal logic

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,6 +4,11 @@ namespace Bakery.Models
 {
   public class Order
   {
-    
+    // GrandTotal Method
+    public static int GetTotal(int subTotal1, int subTotal2)
+    {
+      int total = 0;
+      return total = subTotal1 + subTotal2;
+    }
   }
 }
