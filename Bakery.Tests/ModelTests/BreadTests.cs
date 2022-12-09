e519e4516a4fd9b60a2e34573c9c0fea488879f7
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 09:30:43 -0800

    add tests for subtotal and discount logic

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Bakery.Tests
 {
@@ -21,5 +23,41 @@ namespace Bakery.Tests
       AssertAre.AreEqual(2, breadInstance.Quantity);
     }
 
+    [TestMethod]
+    public void GetSubtotal_GetPriceForOne_Price()
+    {
+      // Based on unit price ($5 per loaf) stored in object, calc base price 
+      Bread breadInstance = new Bread(1);
+      Assert.AreEqual(10, breadInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForTwo_Price()
+    {
+      // Based on unit price ($5 per loaf) stored in object, calc base price 
+      Bread breadInstance = new Bread(2);
+      Assert.AreEqual(10, breadInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForThree_Price()
+    {
+      // Every third loaf is free
+      Bread breadInstance = new Bread(3);
+      Assert.AreEqual(10, breadInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForQuantititesFourToTen_Price()
+    {
+      // Every third loaf is free
+      int[] quantities = {4,5,6,7,8,9,10};
+      foreach (int qty in quantities)
+      {
+        Bread breadInstance = new Bread(qty);
+        Assert.AreEqual(10, breadInstance.SubTotal());
+      }
+
+    }
   }
 }
