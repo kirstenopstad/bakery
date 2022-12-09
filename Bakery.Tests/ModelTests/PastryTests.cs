281b2f2bd553c64e411ccaa0f8e9f2c2cc043d96
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 10:53:28 -0800

    RED: Pastry tests for subtotal and discount logic good failing

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,11 +12,61 @@ namespace Bakery.Tests
       Pastry pastryInstance = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), pastryInstance.GetType());
     }
+
     [TestMethod]
     public void GetQuantity_SetPastryQuantity_Quantity()
     {
       Pastry pastryInstance = new Pastry(1);
       Assert.AreEqual(1, pastryInstance.Quantity);
     }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForOne_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(1);
+      Assert.AreEqual(2, pastryInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForTwo_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(2);
+      Assert.AreEqual(4, pastryInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForThree_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(3);
+      Assert.AreEqual(5, pastryInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForFour_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(4);
+      Assert.AreEqual(7, pastryInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForFive_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(5);
+      Assert.AreEqual(9, pastryInstance.SubTotal());
+    }
+
+    [TestMethod]
+    public void GetSubtotal_GetPriceForSix_Price()
+    {
+      // Based on unit price ($2 per pastry) stored in object & qty
+      Pastry pastryInstance = new Pastry(6);
+      Assert.AreEqual(10, pastryInstance.SubTotal());
+    }
+
   }
 }
