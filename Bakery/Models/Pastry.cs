2c5ee548e5a94ec3040615f1c134a164f0a7b4ee
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 10:45:52 -0800

    passes tests for instantiation and constructor

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,14 @@ namespace Bakery.Models
 {
   public class Pastry
   {
+    public int Quantity { get; set; }
+
+    public Pastry(int qty)
+    {
+      Quantity = qty;
+    }
     // properties, constructors, methods, etc. go here
+
+    
   }
 }
