801fd43515f035422a22ee53abd23ba05d15d24c
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 13:30:07 -0800

    WIP: add validation to user input

--- Bakery/Bakery.cs
+++ Bakery/Bakery.cs
@@ -22,17 +22,52 @@ namespace Bakery
         // Get Bread Order
         Console.WriteLine("How many loaves of bread? (press Enter for none)");
         string breadOrder = Console.ReadLine();
+        if (!IsNumber(breadOrder))
+        {
+          Console.WriteLine("Please enter a valid number.");
+        } 
         // Get Pastry Order
         Console.WriteLine("How many pastries? (press Enter for none)");
         string pastryOrder = Console.ReadLine();
-        // Get Total
+        // Handle user entering nothing
+        if  (breadOrder == "" && pastryOrder == "")
+        {
+          Console.WriteLine("Changed your mind! No order, gotcha.");
+          return;
+        } 
+        else if (pastryOrder == "")
+        {
+          Console.WriteLine("No pastry order, gotcha.");
+          return;
+        } 
+        else if (breadOrder == "")
+        {
+          Console.WriteLine("No bread order, gotcha.");
+          return;
+        }
+        // Validate input
         int breadQty = int.Parse(breadOrder);
         int pastryQty = int.Parse(pastryOrder);
+        // Get Total
         Bread bread = new Bread(breadQty);
         Pastry pastry = new Pastry(pastryQty);
         int total = Pastry.GrandTotal(bread.SubTotal(), pastry.SubTotal());
         Console.WriteLine($"Thanks for your order of {breadQty} loaves of bread and {pastryQty} pastries! Your total is ${total}.");
       }
+    }
+
+    private static bool IsNumber(string userInput)
+    {
+      try
+      {
+        int.Parse(userInput);
+      }
+      catch
+      {
+        Console.WriteLine("Changed your mind! No order, gotcha.");
+        throw;
+      }
+      return true;
     }
   }
 }
