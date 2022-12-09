d654b8f64e1f833065d94c36756db5927413a8b8
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 15:41:08 -0800

    refactor tests to move GetTotal to Order class

---
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void CreateInstance_CreateInstanceOfOrder_Order()
+    {
+      Order orderInstance = new Order();
+      Assert.AreEqual(typeof(Order), orderInstance.GetType());
+    }
+    
+    [TestMethod]
+    public void GetTotal_AddTwoSubTotals_Total()
+    {
+      // Add two subtotal values
+      int breadSubTotal = 5;
+      int pastrySubTotal = 10;
+      Assert.AreEqual(15, Order.Total(breadSubTotal,pastrySubTotal));
+  }
+  
+}
