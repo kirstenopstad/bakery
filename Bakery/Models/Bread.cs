6a205d7128c65e5af80982ce9525d83235e136ca
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 09:59:38 -0800

    passes tests to return prices without discount

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,27 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    // properties, constructors, methods, etc. go here
+    public int Quantity { get; set; }
+    private int _price = 5;
+
+    // Constructor
+    public Bread(int qty)
+    {
+      Quantity = qty;
+    }
+
+    // SubTotal Method
+    public int SubTotal()
+    {
+      int sub = 0;
+      int basePrice = GetBasePrice();
+      return sub = basePrice;
+    }
+
+    private int GetBasePrice()
+    {
+      return Quantity * _price;
+    }
+
   }
 }
