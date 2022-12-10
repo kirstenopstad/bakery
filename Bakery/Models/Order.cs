ef8805e41f1f62c82c4e0c0bfc2de6e32f7b7fe9
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-09 16:01:43 -0800

    GREEN: passes test to apply discount for  orders over

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -8,7 +8,21 @@ namespace Bakery.Models
     public static int GetTotal(int subTotal1, int subTotal2)
     {
       int total = 0;
-      return total = subTotal1 + subTotal2;
+      total = subTotal1 + subTotal2;
+      total = total - GetOrderDiscount(total);
+      return total;
     }
+
+    // Discount Code
+    private static int GetOrderDiscount(int total)
+    {
+      int discount = 0;
+      if (total > 25)
+      {
+        discount = 5;
+      }
+      return discount;
+    }
+    
   }
 }
